9df1cc07f83c35c4137b5f49567bc737d62090a7
BrunoLeiteProgramador <brunohsleite@gmail.com>
2025-09-09 18:08:48 +0000

    carta 1 feita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 
+int main(){
+char Estado;
+char codigo_da_carta [50];
+char nome_da_cidade [50];
+int população;
+float area_em_km²;
+float pib;
+int numeros_de_pontos_turisticos;
+
+// Cadastro da Carta 1
+Printf("Carta 1: Brasil \n");
+printf("Estado: A \n");
+scanf("%c", &Estado);
+
+printf("A01: \n");
+scanf("%s", &codigo_da_carta);
+
+printf("Nome da cidade: Brasilia \n");
+scanf("%s",&nome_da_cidade);
+
+printf("População: 121000 \n");
+scanf("f", &população);
+
+printf("Área: 145.55 KM² \n");
+scanf("f", &area_em_km²);
+
+printf("PIB: 444.88 bilhões de reais \n");
+scanf("f", &pib);
+
+printf("Números de Pontos Turísticos: 38 \n");
+scanf("d", &numeros_de_pontos_turisticos);
+
+}
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
